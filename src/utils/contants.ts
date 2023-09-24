91eaaf5aaa908a0aa89a7fb7e0d55ecd759b0854
Max Blancarte <blancarte@gmail.com>
2023-09-24 15:36:08 -0700

    fix: refactor images

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,9 +1,13 @@
+import woman1 from '../assets/woman_01.png'
+import woman2 from '../assets/woman_02.png'
+import woman3 from '../assets/woman_03.png'
+
 export const event = [
 	{
 		id: 1,
 		description:
 			'La razón de por que cualquier persona puede conseguir el peso y la talla con la que sueñan.',
-		image: 'woman_01.png',
+		image: woman1,
 		heigth: 300,
 		width: 200
 	},
@@ -11,7 +15,7 @@ export const event = [
 		id: 2,
 		description:
 			'Porqué el solo comer y hacer ejercicio, ya no funciona y no esta siendo suficiente.',
-		image: 'woman_02.png',
+		image: woman2,
 		heigth: 300,
 		width: 300
 	},
@@ -19,7 +23,7 @@ export const event = [
 		id: 3,
 		description:
 			'La formula tripié y por que es la única que necesitas para ganarte y crear el cuerpo que anhelas.',
-		image: 'woman_03.png',
+		image: woman3,
 		heigth: 300,
 		width: 300
 	}
