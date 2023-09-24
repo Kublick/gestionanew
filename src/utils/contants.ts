3126d422392bf4922c0828741a42c8924a987e38
Max Blancarte <max.blancarte@digitalvaultstudio.com>
2023-09-24 14:03:30 -0700

    feat: initial layout

---
+++ src/utils/contants.ts
@@ -0,0 +1,26 @@
+export const event = [
+	{
+		id: 1,
+		description:
+			'La razón de por que cualquier persona puede conseguir el peso y la talla con la que sueñan.',
+		image: 'woman_01.png',
+		heigth: 300,
+		width: 200
+	},
+	{
+		id: 2,
+		description:
+			'Porqué el solo comer y hacer ejercicio, ya no funciona y no esta siendo suficiente.',
+		image: 'woman_02.png',
+		heigth: 300,
+		width: 300
+	},
+	{
+		id: 3,
+		description:
+			'La formula tripié y por que es la única que necesitas para ganarte y crear el cuerpo que anhelas.',
+		image: 'woman_03.png',
+		heigth: 300,
+		width: 300
+	}
+]
