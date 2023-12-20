9537e7f4b4e31696dd7457e039f5d38967451552
kublick <blancarte@gmail.com>
2023-12-19 22:56:22 -0800

    feat: Add new assets and update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#B3A7D9',
+				secondary: '#EAE6F4',
+				accent: '#6D8C82',
+				whatsapp: '#25d366'
+			},
+			fontFamily: {
+				bebas: ['Bebas Neue', 'sans-serif'],
+				montserrat: ['Montserrat', 'serif'],
+				'montserrat-italic': ['Montserrat Italic', 'serif']
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 }
